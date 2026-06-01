aabef0d230a2429d75c698574f219ae299777712
Jordan98-San <jordan.sanchez@udla.edu.ec>
2026-06-01 00:08:42 +0000

    Agregar ingreso de calificaciones

--- src/main.c
+++ src/main.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    int estudiantes;
+  int estudiantes;
 
-    printf("Ingrese el numero de estudiantes: ");
-    scanf("%d", &estudiantes);
+  printf("Ingrese el numero de estudiantes: ");
+  scanf("%d", &estudiantes);
 
-    float calificaciones[estudiantes][3];
+  float calificaciones[estudiantes][3];
 
-    printf("\nSistema de gestion de calificaciones\n");
+  printf("\nSistema de gestion de calificaciones\n");
+  for (int i = 0; i < estudiantes; i++) {
+    printf("\nEstudiante %d: \n", i + 1);
+    for (int j = 0; j < 3; i++) {
+      printf("Ingrese las calificaciones de la asignatura %d:", j + 1);
+      scanf("%f", &calificaciones[i][j]);
+    }
+  }
 
-    return 0;
+  return 0;
 }
