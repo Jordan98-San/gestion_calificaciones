edfb6a6b3dd7405f5492912383e2237de6f0f72a
LuisOrozco2007 <luis.orozco.villacis@udla.edu.ec>
2026-05-31 19:41:01 -0500

    Validar rango de calificaciones para cada uno de los estudiantes usando do while - Luis Orozco

--- src/main.c
+++ src/main.c
@@ -1,21 +1,30 @@
 #include <stdio.h>
 
 int main() {
-  int estudiantes;
+    int estudiantes;
 
-  printf("Ingrese el numero de estudiantes: ");
-  scanf("%d", &estudiantes);
+    printf("Ingrese el numero de estudiantes: ");
+    scanf("%d", &estudiantes);
 
-  float calificaciones[estudiantes][3];
+    float calificaciones[estudiantes][3];
 
-  printf("\nSistema de gestion de calificaciones\n");
-  for (int i = 0; i < estudiantes; i++) {
-    printf("\nEstudiante %d: \n", i + 1);
-    for (int j = 0; j < 3; i++) {
-      printf("Ingrese las calificaciones de la asignatura %d:", j + 1);
-      scanf("%f", &calificaciones[i][j]);
+    printf("\nSistema de gestion de calificaciones\n");
+
+    for (int i = 0; i < estudiantes; i++) {
+        printf("\nEstudiante %d\n", i + 1);
+
+        for (int j = 0; j < 3; j++) {
+            do {
+                printf("Ingrese la calificacion de la asignatura %d: ", j + 1);
+                scanf("%f", &calificaciones[i][j]);
+
+                if (calificaciones[i][j] < 0 || calificaciones[i][j] > 10) {
+                    printf("Error: la calificacion debe estar entre 0 y 10.\n");
+                }
+
+            } while (calificaciones[i][j] < 0 || calificaciones[i][j] > 10);
+        }
     }
-  }
 
-  return 0;
+    return 0;
 }
