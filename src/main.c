5a8beb9f10a1b7150ea50299c49584c4954f4943
LuisOrozco2007 <luis.orozco.villacis@udla.edu.ec>
2026-05-31 20:58:17 -0500

    Calcular resultados por asignatura usando ciclos for y condiciones - Luis Orozco

--- src/main.c
+++ src/main.c
@@ -1,54 +1,84 @@
 #include <stdio.h>
 
 int main() {
-  int estudiantes;
+    int estudiantes;
 
-  printf("Ingrese el numero de estudiantes: ");
-  scanf("%d", &estudiantes);
+    printf("Ingrese el numero de estudiantes: ");
+    scanf("%d", &estudiantes);
 
-  float calificaciones[estudiantes][3];
+    float calificaciones[estudiantes][3];
 
-  printf("\nSistema de gestion de calificaciones\n");
+    printf("\nSistema de gestion de calificaciones\n");
 
-  for (int i = 0; i < estudiantes; i++) {
-    printf("\nEstudiante %d\n", i + 1);
+    for (int i = 0; i < estudiantes; i++) {
+        printf("\nEstudiante %d\n", i + 1);
 
-    for (int j = 0; j < 3; j++) {
-      do {
-        printf("Ingrese la calificacion de la asignatura %d: ", j + 1);
-        scanf("%f", &calificaciones[i][j]);
+        for (int j = 0; j < 3; j++) {
+            do {
+                printf("Ingrese la calificacion de la asignatura %d: ", j + 1);
+                scanf("%f", &calificaciones[i][j]);
 
-        if (calificaciones[i][j] < 0 || calificaciones[i][j] > 10) {
-          printf("Error: la calificacion debe estar entre 0 y 10.\n");
+                if (calificaciones[i][j] < 0 || calificaciones[i][j] > 10) {
+                    printf("Error: la calificacion debe estar entre 0 y 10.\n");
+                }
+
+            } while (calificaciones[i][j] < 0 || calificaciones[i][j] > 10);
+        }
+    }
+
+    printf("\n========== RESULTADOS POR ESTUDIANTE ==========\n");
+
+    for (int i = 0; i < estudiantes; i++) {
+        float suma = 0;
+        float mayor = calificaciones[i][0];
+        float menor = calificaciones[i][0];
+
+        for (int j = 0; j < 3; j++) {
+            suma += calificaciones[i][j];
+
+            if (calificaciones[i][j] > mayor) {
+                mayor = calificaciones[i][j];
+            }
+
+            if (calificaciones[i][j] < menor) {
+                menor = calificaciones[i][j];
+            }
         }
 
-      } while (calificaciones[i][j] < 0 || calificaciones[i][j] > 10);
+        float promedio = suma / 3;
+
+        printf("\nEstudiante %d", i + 1);
+        printf("\nPromedio: %.2f", promedio);
+        printf("\nCalificacion mas alta: %.2f", mayor);
+        printf("\nCalificacion mas baja: %.2f\n", menor);
     }
-  }
 
-  printf("################## Resultado por estudiantes ######################");
-  for (int i = 0; i < estudiantes; i++) {
-    float suma = 0;
-    float mayor = calificaciones[i][0];
-    float menor = calificaciones[i][0];
+    printf("\n========== RESULTADOS POR ASIGNATURA ==========\n");
 
-    for (int j = 0; j < 3; i++) {
-      suma += calificaciones[i][j];
+    for (int j = 0; j < 3; j++) {
+        float suma = 0;
+        float mayor = calificaciones[0][j];
+        float menor = calificaciones[0][j];
 
-      if (calificaciones[i][j] > mayor) {
-        mayor = calificaciones[i][j];
-      }
+        for (int i = 0; i < estudiantes; i++) {
+            suma += calificaciones[i][j];
 
-      if (calificaciones[i][j] < menor) {
-        menor = calificaciones[i][j];
-      }
+            if (calificaciones[i][j] > mayor) {
+                mayor = calificaciones[i][j];
+            }
+
+            if (calificaciones[i][j] < menor) {
+                menor = calificaciones[i][j];
+            }
+        }
+
+        float promedio = suma / estudiantes;
+
+        printf("\nAsignatura %d", j + 1);
+        printf("\nPromedio: %.2f", promedio);
+        printf("\nCalificacion mas alta: %.2f", mayor);
+        printf("\nCalificacion mas baja: %.2f\n", menor);
     }
-    float promedio = suma / 3;
 
-    printf("\nEstudiante %d: ", i + 1);
-    printf("\nPromeedio: %2.f", promedio);
-    printf("\nCalificacion mas alta: %2.f", mayor);
-    printf("\nCalificaion mas baja: %2.f", menor);
-  }
-  return 0;
+    return 0;
 }
